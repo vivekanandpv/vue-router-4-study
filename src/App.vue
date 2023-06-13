47492720956e38020b0fe66574b0f159dd9dc63f
Vivekanand P V <vivekanandpv@outlook.com>
2023-06-13 13:02:14 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
 <template>
   <div class="p-5">
     <h1>Vue App</h1>
+    <ul>
+      <li><router-link to="/home">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/news">News</router-link></li>
+      <li><router-link to="/foo">Foo</router-link></li>
+    </ul>
     <hr />
     <router-view></router-view>
   </div>
