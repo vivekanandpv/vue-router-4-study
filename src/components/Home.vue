599edf9ba035b17b598daf800f4a0a3475ecbd37
Vivekanand P V <vivekanandpv@outlook.com>
2024-01-18 10:11:56 +0530

    notes added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-import { router } from '../routes';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const navigationHandler = () => {
   //  router.push('/news/2023')
   router.push({
     path: `/news/${2023}`,
   });
+
+  //  go forward by 1 record (any positive integer)
+  //  router.go(1);   //  same as router.forward();
+
+  // go back by 1 record (any negative integer),
+  //  router.go(-1);  //  same as router.back();
+
+  //  These fail silently if there aren't that many records!
+  //  router.go(-999);
+  //  router.go(10000);
+
+  //  push, replace, go are the counterparts of the window.history API's
+  //  pushState, replaceState, and go
+  //  https://developer.mozilla.org/en-US/docs/Web/API/History_API
 };
 </script>
 <template>
